f5001fdbebcae4e6397eda5f1db32504692a7770
Darmstadti-um <106447442+Darmstadti-um@users.noreply.github.com>
2023-11-25 15:46:27 +0300

    Add files via upload
    Draft code / part of the lect

---
+++ death1.ino
@@ -0,0 +1,23 @@
+void acsel (byte stp, tsv) {
+  for (byte i = 0; i < tsv; i = i + stp) {
+  analogWrite (EN1, i);
+  analogWrite (EN2, i);
+}
+}
+
+
+// функция вызывается перед стартом внутри каждого case
+
+acsel (stp = 10, tsv = 25);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
